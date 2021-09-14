7f5ab8c079229592fbe5ff0ac74cabe947f22e33
PeterNime <63685699+PeterNime@users.noreply.github.com>
2021-09-14 16:25:37 +1200

    Third Submission
    Tuesday

---
+++ AnnimationBlog/Assets/Animations/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator animator;
+    float velocityZ = 0.0f;
+    float velocityX = 0.0f;
+    public float acceleration = 2.0f;
+    public float decleration = 2.0f;
+    public float maxWalkVelocity = 0.02f;
+    public float maxRunVelocity = 2.0f;
+    int VelocityZHash;
+    int VelocityXHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        VelocityZHash = Animator.StringToHash("VelocityZ");
+        VelocityXHash = Animator.StringToHash("VelocityX");
+
+    }
+
+
+    void Update()
+    {
+        bool fordwardPressed = Input.GetKey(KeyCode.W);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+
+        float currentMaxVelocity = runPressed ? maxRunVelocity : maxWalkVelocity;
+
+        if (fordwardPressed && !runPressed && velocityZ < currentMaxVelocity)
+        {
+            velocityZ += Time.deltaTime * acceleration;
+        }
+
+        if (leftPressed && !runPressed && velocityX > -currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * acceleration;
+        }
+
+        if (rightPressed && !runPressed && velocityX < currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * acceleration;
+        }
+
+        if (!rightPressed && !leftPressed && velocityX != 0.0f && velocityX > -0.2f && velocityX < 0.2f)
+        {
+            velocityX = 0.0f;
+        }
+
+        animator.SetFloat(VelocityZHash, velocityZ);
+        animator.SetFloat(VelocityXHash, velocityX);
+
+    }
+
+    void ChangeVelocity(bool forwardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity)
+    {
+
+    }
+
+    void ResetVelocity(bool fowardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity)
+    {
+
+    }
+}
